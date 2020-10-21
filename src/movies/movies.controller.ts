e7b39922ecdc76dfb8c0d122dc2ff0318b29787b
DDI0720 <rlaskdn3615@gmail.com>
2020-10-21 12:38:12 +0900

    add two decorators: Post, Delete

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
     @Get()
-    getAll(){
+    getAll() {
         return 'This return all of the movie lists that we have';
     }
 
     @Get('/:movieId')
-    getOne(@Param('movieId') id: string){
+    getOne(@Param('movieId') id: string) {
         return `You want to search '${id}' ?`;
+    }
+
+    @Post()
+    create() {
+        return 'This will create a movie';
+    }
+
+    @Delete('/:movieId')
+    delete(@Param('movieId') id: string) {
+        return `This removes a movie with the Id ${id}`;
     }
 }
