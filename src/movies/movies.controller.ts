b5280d1369808c189e9b91d190742a00f310c5d5
DDI0720 <rlaskdn3615@gmail.com>
2020-10-21 12:31:34 +0900

    add two decorators: getAll, getOne

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+    @Get()
+    getAll(){
+        return 'This return all of the movie lists that we have';
+    }
+
+    @Get('/:movieId')
+    getOne(@Param('movieId') id: string){
+        return `You want to search '${id}' ?`;
+    }
+}
