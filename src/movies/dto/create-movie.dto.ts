d95fbc143853ff3aa2df0c36a4d89a36751015e9
DDI0720 <rlaskdn3615@gmail.com>
2020-11-01 20:57:11 +0900

    add class-validator

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,5 +1,12 @@
+import { IsString, IsNumber } from "class-validator"
+
 export class CreateMovieDTO {
+    @IsNumber()
     readonly year: number;
+
+    @IsString()
     readonly title: string;
+    
+    @IsString({each: true})
     readonly genres: string[];
 }
