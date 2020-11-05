a1aa9f1202e87264ef666b92722236c19ff1601d
DDI0720 <rlaskdn3615@gmail.com>
2020-11-05 23:14:38 +0900

    add importing movies controllers and providers to movies.module instead of app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
 import { MoviesModule } from './movies/movies.module';
 @Module({
   imports: [MoviesModule],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
