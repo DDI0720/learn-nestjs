c8360caf69b2e59bba529698842f968d38daca02
DDI0720 <rlaskdn3615@gmail.com>
2020-11-09 20:22:31 +0900

    testing

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { arrayContains, ArrayNotEmpty, arrayNotEmpty } from 'class-validator';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
@@ -15,4 +16,25 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe("getAll()", () => {
+    it ("should return array", () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    }) 
+  })
+
+  describe("getOne()", () => {
+    it("should return a movie", () => {
+      service.create({
+        title: "movie name",
+        genres: ["thriller"],
+        year: 2020
+      })
+
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    })
+  })
 });
